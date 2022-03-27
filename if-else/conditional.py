ee4e399456bbcdd42563dc863868e0e9022b7809
Unscop <myratbek-99@mailru>
2022-03-27 14:52:39 +0600

    if-else

--- if-else/conditional.py
+++ if-else/conditional.py
@@ -1,28 +1,28 @@
 import random
 
+
 def is_positive(num):
     if (num > 0):
-        return true
+        return True
     else:
-        return false
+        return False
 
 def is_even(num):
     if (num%2 == 0):
-        return true
+        return True
     else:
-        return false
+        return False
 
 
 def is_positive_and_even(num):
     if (num > 2 and num%2 == 0):
-        return true
+        return True
     else:
-        return false
+        return False
 
 
 def is_positive_or_even(num):
     if (num > 2 or num%2 == 0):
-        return true
+        return True
     else:
-        return false
-
+        return False
